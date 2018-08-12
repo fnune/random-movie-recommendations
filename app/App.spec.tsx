cf730a0ab52e71d535142ca7a85828e050bf72c9
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2018-08-12 16:30:14 +0200

    ✨ Display two movies and make one clickable to continue

--- app/App.spec.tsx
+++ app/App.spec.tsx
@@ -1,16 +1,43 @@
 import { shallow } from 'enzyme'
 import * as React from 'react'
 
+import ShowMovieCard from '~components/ShowMovieCard'
+
 import { App } from './App'
 
 describe('App', () => {
   const fetchMovies = jest.fn() as any
   const fetchStartupData = jest.fn() as any
-  const wrapper = shallow(
-    <App movies={[]} fetchStartupData={fetchStartupData} fetchMovies={fetchMovies} />,
+
+  const spies = [fetchMovies, fetchStartupData]
+
+  const wrapper: any = shallow(
+    <App fetchMovies={fetchMovies} fetchStartupData={fetchStartupData} />,
   )
+
+  beforeEach(() => {
+    spies.forEach(mock => mock.mockReset())
+  })
 
   it('renders', () => {
     expect(wrapper.length).toBe(1)
+  })
+
+  it('renders a ShowMovieCard', () => {
+    expect(wrapper.find(ShowMovieCard)).toHaveLength(1)
+  })
+
+  describe('on mount', () => {
+    it('calls fetchMovies', () => {
+      wrapper.instance().componentDidMount()
+
+      expect(fetchMovies).toHaveBeenCalledWith({})
+    })
+
+    it('calls fetchStartupData', () => {
+      wrapper.instance().componentDidMount()
+
+      expect(fetchStartupData).toHaveBeenCalledWith({})
+    })
   })
 })
--- app/components/MovieCard.tsx
+++ app/components/MovieCard.tsx
@@ -4,15 +4,17 @@ import { fromNullable } from 'fp-ts/lib/Option'
 import Image from './Image'
 
 interface Props {
+  pass?: () => void
   movie: Movie
 }
 
 export class MovieCard extends React.Component<Props> {
   render() {
+    const { movie, pass } = this.props
     return (
-      <div>
-        <h3>{this.props.movie.title}</h3>
-        <Image path={fromNullable(this.props.movie.poster_path)} size="w500" />
+      <div onClick={pass}>
+        <h3>{movie.title}</h3>
+        <Image path={fromNullable(movie.poster_path)} size="w500" />
       </div>
     )
   }
